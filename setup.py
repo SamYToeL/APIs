c0ed25ee9c6416efba10d89767e3cd0760d869da
SamYToeL <lmasgne@gmail.com>
2024-07-19 15:17:06 +0800

    main scripts

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='APIs',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # 任何依赖项都在这里列出
+    ],
+    author='Sam Yunteng Liao',
+    author_email='lmasgne@gmail.com',
+    description='APIs for Gaussian running other calculators through External',
+    license='MIT',
+    keywords='Gaussian--external python APIs',
+    url='https://github.com/SamYToeL/APIs'
+)
